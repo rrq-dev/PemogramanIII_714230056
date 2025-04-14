8ade72a18c9d2d59781e83814716bfcf68434653
rrq-dev <raihanhermawan85@gmail.com>
2025-04-14 15:44:01 +0700

    CODE: update pertemuan 02

---
+++ Pertemuan 02/praktikum/WS_P2_cp/src/App.jsx
@@ -0,0 +1,11 @@
+import Dashboard from "./pages/Dashboard";
+
+function App() {
+  return (
+    <div className="flex flex-col item-center justify-center h-screen gap-4">
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
---
+++ Pertemuan 02/praktikum/WS_P2_cp/src/pages/Dashboard.jsx
@@ -0,0 +1,12 @@
+import TableWithStripRow from "../components/TableWithStripRow"
+
+export default function Dashboard() {
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold text-blue-gray-700 mb-4">
+        Contoh fetch data
+      </h1>
+      <TableWithStripRow />
+    </div>
+  );
+}
